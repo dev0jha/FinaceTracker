bfa4a9f28bcff59a7fcb064e4c9feeb741f553e5
dev0jha <ojha6773@gmail.com>
2026-02-02 13:40:36 +0530

    Refactor date handling and input validation for finance transactions

--- data_entry.py
+++ data_entry.py
@@ -1,20 +1,42 @@
 from datetime import datetime
 
+date_format = "%d-%m-%Y"
+CATEGORIES = {'I':"Income", 'E':"Expense"}
+
+
 def get_date(prompt,allow_default=False): 
     date_str = input(prompt)
     if allow_default and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(date_format)
     try:
-        date_obj = datetime.strptime(date_str, "%d-%m-%Y")
+        date_obj = datetime.strptime(date_str, date_format)
+        return date_obj.strftime(date_format)
     except ValueError:
         print("Invalid date format. Please use DD-MM-YYYY.")
         return get_date(prompt, allow_default)
+
 def get_description(prompt):
-    pass
+    return input ("Enter description: (optional)")
 
 
 def get_amount(prompt):
-    pass
+    try:
+        amount_str = float(input(prompt))
+        if amount_str <= 0:
+            raise ValueError("Amount must be positive.")
+        return amount_str
+    except ValueError as e:
+        print(e)
+        return get_amount()
+
+
 def get_category(prompt, categories):
-    pass
+    category = input("Enter the category('I' for income or 'E' for expense): ").strip().upper()
+    if category in categories:
+        return categories[category]
+    else:
+        print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
+        return get_category(prompt, categories)
+    
+
 
