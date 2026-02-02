bc765627117049d3fdf7c99772135edd8ea0b565
dev0jha <ojha6773@gmail.com>
2026-02-02 14:31:09 +0530

    Refactor data entry methods and enhance CSV handling for finance transactions

--- data_entry.py
+++ data_entry.py
@@ -1,42 +1,41 @@
 from datetime import datetime
 
 date_format = "%d-%m-%Y"
-CATEGORIES = {'I':"Income", 'E':"Expense"}
+CATEGORIES = {"I": "Income", "E": "Expense"}
 
 
-def get_date(prompt,allow_default=False): 
+def get_date(prompt, allow_default=False):
     date_str = input(prompt)
     if allow_default and not date_str:
         return datetime.today().strftime(date_format)
+
     try:
-        date_obj = datetime.strptime(date_str, date_format)
-        return date_obj.strftime(date_format)
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
     except ValueError:
-        print("Invalid date format. Please use DD-MM-YYYY.")
+        print("Invalid date format. Please enter the date in dd-mm-yyyy format")
         return get_date(prompt, allow_default)
 
-def get_description(prompt):
-    return input ("Enter description: (optional)")
 
-
-def get_amount(prompt):
+def get_amount():
     try:
-        amount_str = float(input(prompt))
-        if amount_str <= 0:
-            raise ValueError("Amount must be positive.")
-        return amount_str
+        amount = float(input("Enter the amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a non-negative non-zero value.")
+        return amount
     except ValueError as e:
         print(e)
         return get_amount()
 
 
-def get_category(prompt, categories):
-    category = input("Enter the category('I' for income or 'E' for expense): ").strip().upper()
-    if category in categories:
-        return categories[category]
-    else:
-        print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
-        return get_category(prompt, categories)
-    
+def get_category():
+    category = input("Enter the category ('I' for Income or 'E' for Expense): ").upper()
+    if category in CATEGORIES:
+        return CATEGORIES[category]
+
+    print("Invalid category. Please enter 'I' for Income or 'E' for Expense.")
+    return get_category()
 
 
+def get_descriptipn():
+    return input("Enter a description (optional): ")
